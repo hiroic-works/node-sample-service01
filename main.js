4074d8d370c2ce6e17c09eeb18ea4e6798df6991
h.oda <62193961+hiroic-works@users.noreply.github.com>
2021-08-22 12:44:45 +0900

    MongoDB接続

--- main.js
+++ main.js
@@ -9,6 +9,17 @@ const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+/**
+ * database
+ */
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Connected database'));
+
 app.get('/', (req, res) => {
   res.send('Hello');
 });
