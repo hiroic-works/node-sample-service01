ac620e2e22ce62afdc079cb27e1d59d9e7ecd5af
h.oda <62193961+hiroic-works@users.noreply.github.com>
2021-08-22 12:58:16 +0900

    user modelの定義

---
+++ models/users.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
