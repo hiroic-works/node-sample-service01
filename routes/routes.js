0ef3c6c441647bf4a09b9bc8f6b9a0d935c52a96
h.oda <62193961+hiroic-works@users.noreply.github.com>
2021-08-22 14:38:40 +0900

    フロント側画面作成

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,16 @@ router.get('/', (req, res) => {
   res.render('index', { title: 'Home' });
 });
 
+router.get('/add', (req, res) => {
+  res.render('add-users', { title: 'Add Users' });
+});
+
+router.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact' });
+});
+
 module.exports = router;
